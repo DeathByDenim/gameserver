d78e813814025e0cb12896a34d8203d84ac0cda3
DeathByDenim <jarno@jarno.ca>
2022-07-03 14:54:43 -0400

    Tweak OpenSpades config and preload small maps.

--- scripts/deploy_openspades.sh
+++ scripts/deploy_openspades.sh
@@ -24,7 +24,15 @@ pip install -U piqueserver
 pip install "twisted<21.0.0" # Twisted 22 removed getPage that piqueserver 1.0.0 depends on
 pip install "MarkupSafe==2.0.1" # MarkupSafe removed soft_unicode that piqueserver 1.0.0 depends on
 sudo -u ${systemuser} /opt/openspades/env/bin/piqueserver --copy-config
-sudo -u ${systemuser} sed -i ${systemuserhome}/.config/piqueserver/config.toml -e s/"piqueserver instance"/"onFOSS"/ -e s/"#admin = \[\"adminpass1\", \"adminpass2\"\]"/"admin = \[\"${systempassword}\"\]"/
+sudo -u ${systemuser} sed -i ${systemuserhome}/.config/piqueserver/config.toml \
+  -e s/"piqueserver instance"/"onFOSS"/ \
+  -e s/"#admin = \[\"adminpass1\", \"adminpass2\"\]"/"admin = \[\"${systempassword}\"\]"/ \
+  -e s/"name = \"Blue\""/"name = \"Cyanide\""/ \
+  -e s/"color = \[ 0, 0, 255\]"/"color = [ 0, 255, 255]"/ \
+  -e s/"name = \"Green\""/"name = \"Pinkster\""/ \
+  -e s/"color = \[ 0, 255, 0\]"/"color = [ 255, 0, 255]"/ \
+  -e s/"  \"classicgen\","/"  \"island\",\n  \"pinpoint\",\n  \"realisticbridge\",\n  \"rocketisland\",\n  \"submarine\","/ \
+  -e s/"default_time_limit = \"2hours\""/"default_time_limit = \"20minutes\""/
 deactivate
 
 cat > /etc/systemd/system/openspades.service <<EOF
@@ -40,6 +48,11 @@ User=${systemuser}
 [Install]
 WantedBy=multi-user.target
 EOF
+
+for map in island pinpoint realisticbridge rocketisland submarine; do
+  curl "https://raw.githubusercontent.com/kinvaris/openspades-maps/master/${map}.txt" > ${systemuserhome}/.config/piqueserver/maps/${map}.txt
+  curl "https://raw.githubusercontent.com/kinvaris/openspades-maps/master/${map}.vxl" > ${systemuserhome}/.config/piqueserver/maps/${map}.vxl
+done
 
 systemctl daemon-reload
 systemctl enable --now openspades.service
