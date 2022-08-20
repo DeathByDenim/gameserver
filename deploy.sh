b1e35199c35b94a9712de6e1f31e0203ffa65c7f
DeathByDenim <jarno@jarno.ca>
2022-08-20 08:36:21 -0400

    Added AGPLv3 licence

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+# Collection of scripts to deploy a server hosting several open-source games
+# Copyright (C) 2022  Jarno van der Kolk
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 ### Deploy script for a game server running multiple open-source game servers
 ### This script is intended for Debian 11, but may work on other apt-based
 ### systems too
--- pack_web_offline.sh
+++ pack_web_offline.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 
-# Package a version of the website for offline use on a different web server.
-# Used to indicate that the VM running the game servers is off.
+# Collection of scripts to deploy a server hosting several open-source games
+# Copyright (C) 2022  Jarno van der Kolk
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+### Package a version of the website for offline use on a different web server.
+### Used to indicate that the VM running the game servers is off.
+### Specify domain name:
+###   DOMAINNAME=example.com HOSTEDBYNAME=DeathByDenim ./pack_web_offline.sh
 
 set -e
 
